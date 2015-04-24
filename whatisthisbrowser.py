9ac1e107c54086c2fc7fc430d9f8fa2f8c8a4a0e
Marien Fressinaud <dev@marienfressinaud.fr>
2015-04-24 12:44:51 +0200

    Improve code and coding style
    - PEP8 compliant
    - Python3 compliant
    - Can be used without calling Python command explicitely
    - Show a usage message for bad commands
    - Handle BeautifulSoup4 import error
    - Can be used as a seperate module

--- whatisthisbrowser.py
+++ whatisthisbrowser.py
@@ -1,13 +1,41 @@
-from bs4 import BeautifulSoup
+#!/bin/env python
+# -*- cofing: utf-8 -*-
+
+from __future__ import print_function, unicode_literals
+
 import requests
 import sys
 
-user_agent = sys.argv[1]
-print 'Looking for User-Agent: %s' % user_agent
-
-req = requests.get('https://www.whatismybrowser.com/developers/custom-parse?useragent=%s' % user_agent)
-soup = BeautifulSoup(req.content)
 try:
-	print soup.find('div', attrs={'class': 'string-major'}).text
-except:
-	print 'This has not been found on the website.'
+    from bs4 import BeautifulSoup
+except ImportError as e:
+    sys.exit('Error: BeautifulSoup4 has not been found on your system!')
+
+
+UA_URL = 'https://whatismybrowser.com/developers/custom-parse?useragent={0}'
+
+
+def usage():
+    '''Return a usage message for this program.'''
+    return 'Usage: {cmd} "USER AGENT"'.format(cmd=sys.argv[0])
+
+
+def get_information(user_agent):
+    '''Get and return information about given user agent.'''
+
+    req = requests.get(UA_URL.format(user_agent))
+    soup = BeautifulSoup(req.content)
+
+    try:
+        return soup.find('div', attrs={'class': 'string-major'}).text
+    except AttributeError:
+        return 'This has not been found on the website.'
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(usage())
+
+    user_agent = sys.argv[1]
+    print('Looking for User-Agent: {0}'.format(user_agent))
+    print(get_information(user_agent))
